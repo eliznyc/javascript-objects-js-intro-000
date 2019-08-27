a3573a6e7d7f7403d4f4dcfbe8476c524f785a00
Elizabeth Peters <eliznyc@gmail.com>
2019-08-27 03:45:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,4 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 
-delete playlist.artistName;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
